f298c53dafb1eaf2a73672cefc738169ee4ad75a
Neeraj Patil <neerajpatil009@gmail.com>
2020-10-21 09:09:47 +0530

    Done With The Controllers

---
+++ src/com/DreamTrip/UI/RegisterPage.java
@@ -0,0 +1,41 @@
+package com.DreamTrip.UI;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterPage implements Initializable {
+	@FXML
+	Button navigation_bt;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		navigation_bt.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Parent root = null;
+				try {
+					root = FXMLLoader.load(getClass().getResource("Nav.fxml"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				Scene scene = new Scene(root,800,700);
+				Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+				stage.setScene(scene);
+				stage.setTitle("Search");
+				stage.show();
+			}
+		});
+	}
+}
